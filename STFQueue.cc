580316017d6401771ca6e197abb3043cba93f6f2
Graham Benevelli <grahambenevelli@gmail.com>
2012-02-19 21:17:15 -0600

    Testing

--- STFQueue.cc
+++ STFQueue.cc
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define MAX(x,y) (x) > (y) ? (x) : (y)
+#define MIN(x,y) (x) < (y) ? (x) : (y)
 
 STFQueue::STFQueue() {
 	max_ID = 10;
@@ -12,7 +13,6 @@ STFQueue::STFQueue() {
 	}
 	size = 0;
 	currentTime = 0;
-	currentThreads = 0;
 
 	smutex_init(&Lock);	//init lock
 	scond_init(&fullQueue);	//init Condition variable
@@ -23,14 +23,59 @@ STFQueue::~STFQueue() {
 	smutex_destroy(&Lock);
 	scond_destroy(&fullQueue);
 	// get rid of nodes
+	deleteNodes();
+	delete(t_array);
+}
+
+void
+STFQueue::deleteNodes() {
+	int i;
+	struct thread_list * temp;
+	struct thread_list * delthread;
+	printf("Size: %d\n", size);
+	for (i = 0; i < max_ID; i++) {
+		if(t_array[i] == NULL) continue;
+		delthread = t_array[i];
+		while(delthread->next != NULL) {
+			temp = delthread->next;
+			delthread->next = NULL;
+			delete(delthread);
+			delthread = temp;
+			size--;
+		}
+		delete(delthread);
+		t_array[i] = NULL;
+	}
+	printf("Size: %d\n", size);
+	assert(size == 0);
+}
+
+void
+STFQueue::printQueue() {
+	int i = 0;
+	int j = 0;
+	struct thread_list * temp;
+	for(; i < max_ID; i++) {
+		if(t_array[i] == NULL) continue;
+		temp = t_array[i];
+		printf("Thread %d: ", i);
+		while(temp->next != NULL) {
+			j++;
+			temp = temp->next;
+			printf("(%d node): %lld\t", j, temp->finishTime);
+		}
+		printf("\n");
+		j = 0;
+	}
 }
 
 
-bool STFQueue::enqueue(int flowID, float weight, int lenToSend) {
+bool
+STFQueue::enqueue(int flowID, float weight, int lenToSend) {
 	smutex_lock(&Lock);
 	
 	// resize array if needed
-	if (flowID > max_ID) resizeArray(int flowID);	
+	if (flowID > max_ID) resizeArray(flowID);	
 
 	//check bounds
 	while(size == MAX_SIZE) {
@@ -48,14 +93,15 @@ bool STFQueue::enqueue(int flowID, float weight, int lenToSend) {
 	while (current->next != NULL) current = current->next;
 	assert(current->next == NULL);
 
-	// current is last node
-	current->next = temp;
-
 	//calculate starttime
 	temp->startTime = MAX(current->finishTime, currentTime); // ******************check this
 
 	//calculate finishtime
 	temp->finishTime = temp->startTime + (lenToSend/weight + 0.5); // 0.5 to round up to nearest number
+
+	// current is last node
+	current->next = temp;
+	assert(current->next != NULL);
 
 	//update base finishTime
 	t_array[flowID]->finishTime = temp->finishTime;
@@ -66,16 +112,54 @@ bool STFQueue::enqueue(int flowID, float weight, int lenToSend) {
 	return true;
 }
 
+void
+STFQueue::resizeArray(int id) {
+
+}
+
 int STFQueue::dequeue() {
+	smutex_lock(&Lock);
+
 	// find lowest start time
+	int i;
+	if (size == 0) {
+		printf("IN DEQUEUE() size fail\n");
+		smutex_unlock(&Lock);
+		return -1;
+	}
+
+	int min = 0;
+	for (i = 1; i < max_ID; i++) {
+		if (t_array[min]->next == NULL) {min = i; continue;}
+		if (t_array[i]->next != NULL) {
+			if (t_array[min]->next->startTime > t_array[i]->next->startTime)
+				min = i;
+		}
+	}
+	assert(t_array[min]->next != NULL);
+
 	// save that node
+	struct thread_list *temp = t_array[min]->next;
+
 	// set that slot to next node
-	// delete that node
-	// decrement currentThreads
+	t_array[min]->next = t_array[min]->next->next;
+
+	// decrement size
+	size--;
+
 	// update currentTime
+	currentTime = temp->startTime;	
+
+	// delete that node
+	delete(temp);
+
 	// signal next enqueue
+	scond_signal(&fullQueue, &Lock);
+
+	smutex_unlock(&Lock);
+
 	// return flowID
-	return -1;
+	return min;
 }
 
 
@@ -106,17 +190,157 @@ void STFQueue::unit() {
 	test++;
 
 	// constructor Tests 4
-	result = currentThreads == 0;
+	result = size == 0;
 	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
 	test++;
 
 	enqueue(0, 1, 10);
 
 	// enqueue Tests 5
-	result = t_array[0].next != NULL;
+	result = t_array[0]->next != NULL;
 	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
 	test++;
 
-	if(allPassed)printf("All %d test passed in STFQueue\n", test);
+	// enqueue Tests 6
+	result = t_array[0]->next->startTime == 0;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 7
+	result = t_array[0]->next->finishTime == 10;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 8
+	result = t_array[0]->next->next == NULL;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 9
+	result = size == 1;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 10
+	result = t_array[0]->finishTime == 10;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	enqueue(1, 2, 10);
+
+	// enqueue Tests 11
+	result = t_array[1]->next != NULL;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 12
+	result = t_array[1]->next->startTime == 0;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 13
+	result = t_array[1]->next->finishTime == 5;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 14
+	result = t_array[1]->next->next == NULL;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 15
+	result = size == 2;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 16
+	result = t_array[1]->finishTime == 5;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	enqueue(0, 1, 20);
+
+	// enqueue Tests 17
+	result = t_array[0]->next->next != NULL;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 18
+	result = t_array[0]->next->next->startTime == 10;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 19
+	result = t_array[0]->next->next->finishTime == 30;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 20
+	result = t_array[0]->next->next->next == NULL;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 21
+	result = size == 3;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// enqueue Tests 22
+	result = t_array[0]->finishTime == 30;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	int de = dequeue();
+
+	// dequeue Tests 23
+	result = t_array[0]->next->next == NULL;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// dequeue Tests 24
+	result = de == 0;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// dequeue Tests 25
+	result = t_array[0]->next->startTime == 10;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// dequeue Tests 26
+	result = t_array[0]->next->finishTime == 30;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// dequeue Tests 27
+	result = t_array[0]->finishTime == 30;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// dequeue Tests 28
+	result = currentTime == 0;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+
+	enqueue(4, 3, 10);
+	enqueue(5, 3, 10);
+	enqueue(1, 3, 10);
+	enqueue(4, 3, 10);
+	enqueue(4, 3, 10);
+	printQueue();
+	deleteNodes();
+
+	// delete Tests 29
+	result = t_array[0] == NULL;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	// delete Tests 30
+	result = t_array[1] == NULL;
+	if (!result) {printf("Test %d failed in STFQueue\n", test); allPassed = false;}
+	test++;
+
+	if(allPassed)printf("All %d test passed in STFQueue\n", test-1);
 }
 
--- STFQueue.h
+++ STFQueue.h
@@ -3,27 +3,29 @@
 
 class STFQueue {
 	struct thread_list{
-		int flowId;
 		long long finishTime;
 		long long startTime;
 		thread_list *next;
 	};
 	struct thread_list **t_array;
 	int size;
-	static const int MAX_SIZE = 10;	
+	static const int MAX_SIZE = 100;	
 	int max_ID;	
 	float currentTime;
-	int currentThreads;
 
 	//synchronization variables
 	smutex_t Lock;
 	scond_t fullQueue;
+
+	void resizeArray(int id);
+	void deleteNodes();
 	
 	public:
 	STFQueue();
 	~STFQueue();
 	bool enqueue(int flowID, float weight, int lenToSend);
 	int dequeue();
+	void printQueue();
 	void unit();
 };
 
